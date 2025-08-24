709063994ff7c3a2f2dad2e73eac58db7bdb9bca
Eyalz111 <eyalz111@gmail.com>
2025-08-24 11:09:49 +0300

    Added whisper function to text_tools

--- text_tools.py
+++ text_tools.py
@@ -7,6 +7,10 @@ def shout(text):
     """Returns the text in uppercase"""
     return text.upper()
 
+def whisper(text):
+    """Returns the text in lowercase"""
+    return text.lower()
+
 # This part ONLY runs when the file is run directly!
 if __name__ == "__main__":
     print("Testing count_words:", count_words("Hello world"))
